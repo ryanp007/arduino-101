b235809ee2b15b8dd96254d0da42e33e97247730
ryanp007 <140285621+ryanp007@users.noreply.github.com>
2025-06-26 14:31:47 -0700

    Create led-anim.cpp

---
+++ led-anim.cpp
@@ -0,0 +1,28 @@
+// C++ code
+//
+int animationSpeed = 400;
+
+void setup()
+{ animationSpeed=400; 
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(animationSpeed);
+  digitalWrite(13, LOW);
+  delay(animationSpeed);
+  digitalWrite(12, HIGH);
+  delay(animationSpeed); 
+  digitalWrite(12, LOW);
+  delay(animationSpeed); 
+  digitalWrite(11, HIGH);
+  delay(animationSpeed); 
+  digitalWrite(11, LOW);
+  delay(animationSpeed); 
+  animationSpeed=animationSpeed-50; 
+  if 
+}
